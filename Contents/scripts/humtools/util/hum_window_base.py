38015b3968c8e75ff481b6f2149747e97c1b7585
Hum9183 <hum9183@gmail.com>
2023-06-17 16:50:34 +0900

    ウィンドウのリロードメソッドを追加、他。
    ・不要importを削除。
    ・docstringを追加。

--- Contents/scripts/humtools/util/hum_window_base.py
+++ Contents/scripts/humtools/util/hum_window_base.py
@@ -2,24 +2,37 @@
 from abc import ABCMeta, abstractmethod
 
 from maya import cmds
-from maya.common.ui import LayoutManager
 
 
 class HumWindowBase:
+    """cmdsを用いたGUIウィンドウを使用する場合に継承する基底クラス。"""
     __metaclass__ = ABCMeta
 
     def __init__(self, tool_name):
+        """
+        Args:
+            tool_name (str): ツール名
+        """
         self.tool_name = tool_name
 
     def show(self):
+        """ウィンドウを生成し、表示する"""
         self.__delete_window()
         self._create_window()
         cmds.showWindow()
 
+    def reload_window(self):
+        """ウィンドウをリロードする。"""
+        cmds.evalDeferred(lambda *args: self.show())
+
     @abstractmethod
     def _create_window(self):
+        """ウィンドウを生成する。
+        抽象メソッドであるため、実装はすべて派生クラスに記述する。
+        """
         pass
 
     def __delete_window(self):
+        """ウィンドウを削除する。"""
         if cmds.window(self.tool_name, ex=True):
             cmds.deleteUI(self.tool_name)
