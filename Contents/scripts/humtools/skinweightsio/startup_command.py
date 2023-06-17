4527a7cbb764de1b4e47a436329a490c7d5f64f7
Hum9183 <hum9183@gmail.com>
2023-06-17 18:24:20 +0900

    【SkinWeightsIO】スタートアップコマンドの関数名を「bsme」から「swio」に変更。

--- Contents/scripts/humtools/skinweightsio/startup_command.py
+++ Contents/scripts/humtools/skinweightsio/startup_command.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-def bsme_startup_command():
-    bsme_reload_modules()
-    bsme_show_window()
+def swio_startup_command():
+    swio_reload_modules()
+    swio_show_window()
 
 
-def bsme_reload_modules():
+def swio_reload_modules():
     from humtools.skinweightsio import const, window, selection, auto_skin_binder, mesh_parent_transform_and_skincluster_dict_getter, \
         deformer_weights_exporter, deformer_weights_importer, main, xml_text_scroll_list, option_settings, lang_op_var
 
@@ -15,7 +15,7 @@ def bsme_reload_modules():
     module_reloader.reload_a_few_times(modules)
 
 
-def bsme_show_window():
+def swio_show_window():
     from humtools.skinweightsio.window import Window
     from humtools.skinweightsio.const import Const
     wnd = Window(Const.TOOL_NAME)
@@ -23,4 +23,4 @@ def bsme_show_window():
 
 
 if __name__ == '__main__':
-    bsme_startup_command()
+    swio_startup_command()
