94f1f37af8953c191d89f631ce710b32ec8e303c
Miyakawa Takeshi <117564304+Hum9183@users.noreply.github.com>
2024-12-03 22:25:31 +0900

    Feature/skinweightsio/progress window (#5)
    * 【SkinWeightsIO】XML生成やウェイトコピーの進捗をProgressWindowで出すように
    
    * リフォーマット

--- Contents/scripts/humtools/skinweightsio/deformer_weights_exporter.py
+++ Contents/scripts/humtools/skinweightsio/deformer_weights_exporter.py
@@ -2,23 +2,37 @@
 
 from maya import cmds
 
+from .lang_op_var import LangOpVar
 from ..util.extensions import Extensions
-from ..util.lang       import Lang
-from ..util.log        import Log
-from .lang_op_var      import LangOpVar
+from ..util.lang import Lang
+from ..util.log import Log
+from ..util.progress_window import ProgressWindow
 
 
 class DeformerWeightsExporter:
     def __init__(self, xmls_folder_path, xml_text_scroll_list):
-        self.__xmls_folder_path     = xmls_folder_path
+        self.__xmls_folder_path = xmls_folder_path
         self.__xml_text_scroll_list = xml_text_scroll_list
 
     def export_xmls(self, mesh_parent_transform_and_skincluster_dict):
+        progress_window = ProgressWindow(len(mesh_parent_transform_and_skincluster_dict),
+                                         Lang.pack(u'XMLを生成中...', 'Creating XMLs...', LangOpVar.get()),
+                                         LangOpVar.get())
+        progress_window.show()
+
         for mesh_parent_transform, skin_cluster in mesh_parent_transform_and_skincluster_dict.items():
+            # ProgressWindowの更新処理
+            if progress_window.is_cancelled():
+                break
+            if progress_window.is_greater_than_max():
+                break
+            progress_window.next()
+
             xml_file_name = self.__get_xml_file_name(mesh_parent_transform)
             self.__export_xmls(xml_file_name, skin_cluster)
             self.__log(mesh_parent_transform)
 
+        progress_window.close()
         self.__load_text_scroll_list()
 
     def __get_xml_file_name(self, mesh_parent_transform):
@@ -26,10 +40,10 @@ class DeformerWeightsExporter:
 
     def __export_xmls(self, xml_file_name, skin_cluster):
         cmds.deformerWeights(
-                            xml_file_name,
-                            export=True,
-                            deformer=skin_cluster,
-                            path=self.__xmls_folder_path)
+            xml_file_name,
+            export=True,
+            deformer=skin_cluster,
+            path=self.__xmls_folder_path)
 
     def __log(self, mesh_parent_transform):
         log_msg = Lang.pack(u'{}のXMLを作成しました。'.format(mesh_parent_transform),
--- Contents/scripts/humtools/skinweightsio/deformer_weights_importer.py
+++ Contents/scripts/humtools/skinweightsio/deformer_weights_importer.py
@@ -3,42 +3,59 @@ import os
 
 from maya import cmds
 
+from .lang_op_var import LangOpVar
+from ..util import path
 from ..util.extensions import Extensions
-from ..util.lang       import Lang
-from ..util.log        import Log
-from ..util            import path
-from .lang_op_var      import LangOpVar
+from ..util.lang import Lang
+from ..util.log import Log
+from ..util.progress_window import ProgressWindow
 
 
 class DeformerWeightsImporter:
     def __init__(self, xmls_folder_path, option_settings):
-        self.__xmls_folder_path  = xmls_folder_path
+        self.__xmls_folder_path = xmls_folder_path
         self.__option_settings = option_settings
 
     def import_xmls(self, transform_and_skincluster_dict):
-        xmls_folder_path  = self.__xmls_folder_path
-        import_method     = self.__option_settings.import_method
-        ignore_name       = self.__option_settings.ignore_name
+        xmls_folder_path = self.__xmls_folder_path
+        import_method = self.__option_settings.import_method
+        ignore_name = self.__option_settings.ignore_name
         normalize_weights = self.__option_settings.normalize_weights
+
+        progress_window = ProgressWindow(len(transform_and_skincluster_dict),
+                                         Lang.pack(u'ウェイトをコピー中...', 'Copying weights...', LangOpVar.get()),
+                                         LangOpVar.get())
+        progress_window.show()
 
         # NOTE: xmlと同名のメッシュ(メッシュの親トランスフォーム)に自動でウェイトをコピーする
         for mesh_parent_transform, skin_cluster in transform_and_skincluster_dict.items():
+            # ProgressWindowの更新処理
+            if progress_window.is_cancelled():
+                break
+            if progress_window.is_greater_than_max():
+                break
+            progress_window.next()
+
+            # XMLがあるか確認
             xml_file_name = self.__get_xml_file_name(mesh_parent_transform)
             if self.__exists_xml_file(xmls_folder_path, xml_file_name, mesh_parent_transform) is False:
                 continue
 
+            # ウェイトコピー処理
             cmds.deformerWeights(
-                                xml_file_name,
-                                im=True,
-                                method=import_method,
-                                ignoreName=ignore_name,
-                                deformer=skin_cluster,
-                                path=xmls_folder_path)
+                xml_file_name,
+                im=True,
+                method=import_method,
+                ignoreName=ignore_name,
+                deformer=skin_cluster,
+                path=xmls_folder_path)
             self.__normalize_weights(skin_cluster, normalize_weights)
             log_msg = Lang.pack(u'{}にウェイトをコピーしました。'.format(mesh_parent_transform),
                                 'Copied weights to {}.'.format(mesh_parent_transform),
                                 LangOpVar.get())
             Log.log(log_msg)
+
+        progress_window.close()
         # self.__option_settings.log()
 
     def __get_xml_file_name(self, mesh_parent_transform):
@@ -49,10 +66,11 @@ class DeformerWeightsImporter:
         if os.path.isfile(xml_file_path):
             return True
         else:
-            warning_msg = Lang.pack(u'{}と同名のXMLが存在しないため、ウェイトコピーができませんでした。'.format(mesh_parent_transform),
-                                    'Weight copy could not be performed because XML with the same name as {} does not exist.'.format(mesh_parent_transform),
-                                    LangOpVar.get())
-            Log.warning(warning_msg)
+            ja_JP = u'{}と同名のXMLが存在しないため、ウェイトコピーができませんでした。'.format(mesh_parent_transform)
+            en_US = 'Weight copy could not be performed because XML with the same name as {} does not exist.'.format(
+                mesh_parent_transform)
+            warning_msg = Lang.pack(ja_JP, en_US, LangOpVar.get())
+            Log.warning(warning_msg)  # TODO: Logだけだと気が付きにくいためinViewMessageも出す
             return False
 
     def __normalize_weights(self, skin_cluster, normalize_weights):
